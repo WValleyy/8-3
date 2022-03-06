6cae9a446273f1dd10ddca3cc33c0c22f86f3247
Tiến Doanh Nguyễn <doanhearth99@gmail.com>
2022-03-06 11:08:02 +0700

    message

---
+++ config.js
@@ -0,0 +1,17 @@
+   const CONFIG = {
+        introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+        introDesc: `Tớ có điều này muốn hỏi Cậu, Cậu nhớ phải trả lời thật lòng nha`,
+        btnIntro: 'OK',
+        title: 'Cậu có nhớ 12 Toán 2 không ? 😙',
+        desc: 'Trả lời nhanh lên bruh ',
+        btnYes: 'Khá là muốn gặp mn',
+        btnNo: 'Quên cmnr ',
+        question:'Cậu muốn được chúng mình tặng gì nhân ngày tôn vinh phụ nữ 8/3 nào ',
+        reasonPlaceholder: 'Chỗ để type chữ',
+        btnReply: 'Gửi! 💌💌',
+        reply: 'Một món quà nhỏ do ae Toán 2 tự làm nhé :3',
+        mess: 'Tuy không phải điều gì lớn lao nhưng mong các chị em sẽ thích nó :3',
+        messDesc: 'Nhấp vào đi các bruh',
+        btnAccept: 'Ok ',
+        YtbLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+}
